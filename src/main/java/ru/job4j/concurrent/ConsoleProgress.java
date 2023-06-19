6114abdffe322312d4695e7a553cee5bab3ebcf7
Tatyana <quininthenorth@gmail.com>
2023-06-19 19:46:28 +0400

    Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        String[] process = {"-", "\\", "|", "/"};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[index++ % 4]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt();
+    }
+}
