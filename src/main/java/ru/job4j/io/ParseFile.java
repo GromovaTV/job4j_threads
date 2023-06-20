f41314cb7a42d0f9c50d5c58695ffce5f9573203
Tatyana <quininthenorth@gmail.com>
2023-06-20 22:13:36 +0400

    Visibility. Общий ресурс вне критической секции

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized String getContentWithoutUnicode(Predicate<Character> filter) throws IOException{
+        String output = "";
+        try (BufferedInputStream i = new BufferedInputStream (new FileInputStream(file)))
+        {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+            return output;
+        }
+    }
+}
