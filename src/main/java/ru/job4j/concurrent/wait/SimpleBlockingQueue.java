08ffe37fd12588e38f30d5d0844370fff49e2e07
Tatyana <quininthenorth@gmail.com>
2023-06-21 23:19:54 +0400

    Реализовать шаблон Producer Consumer. Простая блокирующая очередь

---
+++ src/main/java/ru/job4j/concurrent/wait/SimpleBlockingQueue.java
@@ -0,0 +1,50 @@
+package ru.job4j.concurrent.wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final int capacity;
+
+    public SimpleBlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public synchronized Queue<T> getQueue() {
+        return queue;
+    }
+
+    public synchronized void offer(T value) {
+        while (queue.size() == capacity) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        queue.offer(value);
+        this.notifyAll();
+        System.out.println("Добавлен элемент: " + value);
+
+    }
+
+    public synchronized T poll() {
+        while (queue.size() == 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        var res = queue.poll();
+        this.notifyAll();
+        System.out.println("Извлечен элемент: " + res);
+        return res;
+    }
+}
---
+++ src/test/java/ru/job4j/concurrent/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.concurrent.wait;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+    @Test
+    public void when6Offer2Poll() throws InterruptedException {
+        var queue = new SimpleBlockingQueue<Integer>(4);
+        Thread producer = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    queue.offer(1);
+                    queue.offer(2);
+                    queue.offer(3);
+                    queue.offer(4);
+                    queue.offer(5);
+                    queue.offer(6);
+                },
+                "Producer"
+        );
+        Thread consumer = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    queue.poll();
+                    queue.poll();
+                },
+                "Consumer"
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        Queue<Integer> res = queue.getQueue();
+        Queue<Integer> exp = new LinkedList<>(List.of(3, 4, 5, 6));
+        System.out.println(queue.getQueue());
+        assertEquals(exp, res);
+    }
+
+}
