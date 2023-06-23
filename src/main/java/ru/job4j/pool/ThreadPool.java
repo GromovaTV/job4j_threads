3d5b6a8e2e39d45af64409d762d92bf16f81e3ec
Tatyana <quininthenorth@gmail.com>
2023-06-23 21:22:01 +0400

    Реализовать ThreadPool

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,55 @@
+package ru.job4j.pool;
+
+import ru.job4j.concurrent.wait.SimpleBlockingQueue;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(100);
+
+    public void init() {
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int index = 0; index < size; index++) {
+            ThreadWithQueue threadWithQueue = new ThreadWithQueue(tasks);
+            threads.add(threadWithQueue);
+            threadWithQueue.start();
+        }
+    }
+
+    public void work(Runnable job) {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        threads.forEach(Thread::interrupt);
+    }
+
+    static class ThreadWithQueue extends Thread {
+        private final SimpleBlockingQueue<Runnable> tasks;
+
+        public ThreadWithQueue(SimpleBlockingQueue<Runnable> tasks) {
+            this.tasks = tasks;
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                if (tasks.isEmpty()) {
+                    try {
+                        synchronized (this) {
+                            this.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                try {
+                    tasks.poll().run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
